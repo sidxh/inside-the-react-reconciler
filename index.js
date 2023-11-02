3ba90675a49e9ebb885032b35b91ed804ef94ee9
siddhant <101110768+sidxh@users.noreply.github.com>
2023-11-03 03:35:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const todos = [
+    { id: 1, text: "Basically This Is a " },
+    { id: 2, text: "Build a project" },
+    { id: 3, text: "Write code every day" },
+    { id: 4, text: "Okay Got It" },
+    { id: 5, text: "Write code " },
+    { id: 6, text: "Ahahha day" },
+  ];
